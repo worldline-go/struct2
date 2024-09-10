8f5b6aa760dfc79eabab7125d6e74fcea87e1b31
Ron <38083777+roneli@users.noreply.github.com>
2024-09-10 12:15:28 +0300

    fix to support ptrs + tests

--- cast.go
+++ cast.go
@@ -3,15 +3,19 @@ package struct2
 import (
 	"fmt"
 	"html/template"
+	"reflect"
 	"strconv"
 )
 
 // ToStringE casts an interface to a string type.
 // Taken from github.com/spf13/cast. All rights reserved.
 func ToStringE(i interface{}) (string, error) {
+	i = indirectToStringerOrError(i)
 	switch s := i.(type) {
 	case string:
 		return s, nil
+	case fmt.Stringer:
+		return s.String(), nil
 	case bool:
 		return strconv.FormatBool(s), nil
 	case float64:
@@ -52,11 +56,29 @@ func ToStringE(i interface{}) (string, error) {
 		return string(s), nil
 	case nil:
 		return "", nil
-	case fmt.Stringer:
-		return s.String(), nil
 	case error:
 		return s.Error(), nil
 	default:
 		return "", fmt.Errorf("unable to cast %#v of type %T to string", i, i)
 	}
+}
+
+// From html/template/content.go
+// Copyright 2011 The Go Authors. All rights reserved.
+// indirectToStringerOrError returns the value, after dereferencing as many times
+// as necessary to reach the base type (or nil) or an implementation of fmt.Stringer
+// or error,
+func indirectToStringerOrError(a interface{}) interface{} {
+	if a == nil {
+		return nil
+	}
+
+	errorType := reflect.TypeOf((*error)(nil)).Elem()
+	fmtStringerType := reflect.TypeOf((*fmt.Stringer)(nil)).Elem()
+
+	v := reflect.ValueOf(a)
+	for !v.Type().Implements(fmtStringerType) && !v.Type().Implements(errorType) && v.Kind() == reflect.Ptr && !v.IsNil() {
+		v = v.Elem()
+	}
+	return v.Interface()
 }
